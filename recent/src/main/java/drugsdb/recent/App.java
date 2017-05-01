b8665799ef40b8848cda286e1c9b90410073b153
Chinnakotla <Kranthi.Chinnakotla@hughes.com>
2017-05-01 17:04:03 -0400

    modified:   recent/src/main/java/drugsdb/recent/App.java 	new file:   recent/src/main/java/drugsdb/recent/configuration/BatchConfiguration.java 	new file:   recent/src/main/java/drugsdb/recent/configuration/JobCompletionNotificationListener.java 	new file:   recent/src/main/java/drugsdb/recent/controllers/BatchExecution.java 	new file:   recent/src/main/java/drugsdb/recent/controllers/BatchLauncher.java 	modified:   recent/src/main/java/drugsdb/recent/controllers/FileUploadController.java 	new file:   recent/src/main/java/drugsdb/recent/model/Drug.java 	new file:   recent/src/main/java/drugsdb/recent/model/DrugItemProcessor.java 	new file:   recent/src/main/java/drugsdb/recent/model/Specifications.java 	new file:   recent/src/main/java/drugsdb/recent/service/DrugService.java 	deleted:    recent/src/main/java/drugsdb/recent/storage/FileSystemStorageService.java 	deleted:    recent/src/main/java/drugsdb/recent/storage/StorageException.java 	deleted:    recent/src/main/java/drugsdb/recent/storage/StorageFileNotFoundException.java 	deleted:    recent/src/main/java/drugsdb/recent/storage/StorageProperties.java 	deleted:    recent/src/main/java/drugsdb/recent/storage/StorageService.java 	modified:   recent/src/main/resources/templates/uploadForm.html 	modified:   recent/target/classes/drugsdb/recent/App.class 	new file:   recent/target/classes/drugsdb/recent/configuration/BatchConfiguration$1$1.class 	new file:   recent/target/classes/drugsdb/recent/configuration/BatchConfiguration$1$2.class 	new file:   recent/target/classes/drugsdb/recent/configuration/BatchConfiguration$1.class 	new file:   recent/target/classes/drugsdb/recent/configuration/BatchConfiguration.class 	new file:   recent/target/classes/drugsdb/recent/configuration/JobCompletionNotificationListener.class 	new file:   recent/target/classes/drugsdb/recent/controllers/BatchExecution$GetLeaf.class 	new file:   recent/target/classes/drugsdb/recent/controllers/BatchExecution.class 	new file:   recent/target/classes/drugsdb/recent/controllers/BatchLauncher.class 	modified:   recent/target/classes/drugsdb/recent/controllers/FileUploadController.class 	new file:   recent/target/classes/drugsdb/recent/model/Drug.class 	new file:   recent/target/classes/drugsdb/recent/model/DrugItemProcessor$1.class 	new file:   recent/target/classes/drugsdb/recent/model/DrugItemProcessor.class 	new file:   recent/target/classes/drugsdb/recent/model/Specifications.class 	new file:   recent/target/classes/drugsdb/recent/service/DrugService.class 	deleted:    recent/target/classes/drugsdb/recent/storage/FileSystemStorageService.class 	deleted:    recent/target/classes/drugsdb/recent/storage/StorageException.class 	deleted:    recent/target/classes/drugsdb/recent/storage/StorageFileNotFoundException.class 	deleted:    recent/target/classes/drugsdb/recent/storage/StorageProperties.class 	deleted:    recent/target/classes/drugsdb/recent/storage/StorageService.class 	modified:   recent/target/classes/templates/uploadForm.html 	deleted:    recent/uploadingdir/Business Cards on Apr 20, 2017, 3_53_15 PM.pdf 	deleted:    recent/uploadingdir/UserFile.pdf

--- recent/src/main/java/drugsdb/recent/App.java
+++ recent/src/main/java/drugsdb/recent/App.java
@@ -1,8 +1,8 @@
 package drugsdb.recent;
 
 import java.io.File;
+import java.util.ArrayList;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,36 +10,25 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.jms.JndiConnectionFactoryAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-
-import drugsdb.recent.storage.StorageService;
-import drugsdb.recent.storage.StorageProperties;
 import drugsdb.recent.App;
 import drugsdb.recent.controllers.FileUploadController;
+import drugsdb.recent.model.Drug;
 
-/**
- * Hello world!
- *
- */
+
 
 @SpringBootApplication
 @ComponentScan(basePackages="drugsdb.recent")
-@EnableConfigurationProperties(StorageProperties.class)
+@EnableConfigurationProperties()
 @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class,DataSourceTransactionManagerAutoConfiguration.class,JndiConnectionFactoryAutoConfiguration.class})
 public class App 
 {
+	public static ArrayList<Drug> queryList = new ArrayList<Drug>();
     public static void main( String[] args )
     {
     	new File(FileUploadController.uploadingdir).mkdirs();
     	SpringApplication.run(App.class, args);
     }
     
-   /* @Bean
-   	CommandLineRunner init(StorageService storageService) { 
-   		return (args) -> {
-               storageService.deleteAll();
-               storageService.init();
-   		};
-   	}*/
+  
 }
